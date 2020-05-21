55a9db16595c31f2c836645eb04f7b6bd3180668
Diego Arostegui <darostegui@globalint.info>
2020-05-21 22:14:48 +0200

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,14 +1,14 @@
-!/home/ubuntu/.devops/bin/en
+#!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build . --tag=microservices
+docker build . --tag=globalint/microservices
 # Step 2: 
 # List docker images
 docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it --rm --name microservices -p 8000:80 microservices
+docker run -it --rm --name microservices -p 8000:80 globalint/microservices
